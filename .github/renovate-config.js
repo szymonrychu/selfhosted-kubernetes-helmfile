48aa074107fade799f018043bb220c95ec735845
Szymon Richert <szymon.rychu@gmail.com>
2023-08-02 12:56:51 +0200

    Update renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,35 +1,25 @@
 module.exports = {
-    dryRun: false,
-    username: 'szymonrichert.pl bot',
-    gitAuthor: 'szymonrichert.pl bot <bot@szymonrichert.pl>',
-    onboarding: false,
-    platform: 'github',
-    repositories: [
-      'szymonrychu/helmfile-cluster',
-    ],
-    packageRules: [
-      {
-        dependencyDashboardApproval: false,
-        description: 'MinorAutoUpgrade',
-        matchUpdateTypes: [
-          'pin',
-          'digest',
-          'patch',
-          'minor',
-          'major'
-        ],
-        automerge: true,
-        addLabels: ["automerge"],
-        prCreation: "not-pending",
-        stabilityDays: 0,
-      },
-      {
-        dependencyDashboardApproval: false,
-        description: 'MajorUpgrade',
-        matchUpdateTypes: [
-          'major'
-        ],
-        stabilityDays: 7,
-      },
-    ],
+    "dryRun": false,
+     "username": "szymonrichert.pl bot",
+     "gitAuthor": "szymonrichert.pl bot <bot@szymonrichert.pl>",
+     "onboarding": false,
+     "platform": "github",
+     "repositories": ["szymonrychu/helmfile-cluster"],
+     "packageRules": [
+       {
+         "dependencyDashboardApproval": false,
+         "description": "MinorAutoUpgrade",
+         "matchUpdateTypes": ["pin", "digest", "patch", "minor", "major"],
+         "automerge": true,
+         "addLabels": ["automerge"],
+         "prCreation": "not-pending",
+         "stabilityDays": 0
+       },
+       {
+         "dependencyDashboardApproval": false,
+         "description": "MajorUpgrade",
+         "matchUpdateTypes": ["major"],
+         "stabilityDays": 7
+       }
+     ]
   };
