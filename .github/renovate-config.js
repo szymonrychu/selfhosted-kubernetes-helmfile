dfd956918d648661f47e59ec5dbdf0cc63e0c811
Szymon Richert <szymon.rychu@gmail.com>
2023-11-02 13:59:04 +0100

    Migrate renovate config

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,20 +1,20 @@
 module.exports = {
-     "username": "szymonrichert.pl bot",
-     "gitAuthor": "szymonrichert.pl bot <bot@szymonrichert.pl>",
-     "onboarding": false,
-     "platform": "github",
-     "repositories": ["szymonrychu/helmfile-cluster"],
-     "dependencyDashboard": false,
-     "packageRules": [
-       {
-         "description": "MinorAutoUpgrade",
-         "matchUpdateTypes": ["pin", "digest", "patch", "minor", "major"],
-         "stabilityDays": 0
-       },
-       {
-         "description": "MajorUpgrade",
-         "matchUpdateTypes": ["major"],
-         "stabilityDays": 7
-       }
-     ]
-  };
+  "username": "szymonrichert.pl bot",
+  "gitAuthor": "szymonrichert.pl bot <bot@szymonrichert.pl>",
+  "onboarding": false,
+  "platform": "github",
+  "repositories": ["szymonrychu/helmfile-cluster"],
+  "dependencyDashboard": false,
+  "packageRules": [
+    {
+      "description": "MinorAutoUpgrade",
+      "matchUpdateTypes": ["pin", "digest", "patch", "minor", "major"],
+      "minimumReleaseAge": null
+    },
+    {
+      "description": "MajorUpgrade",
+      "matchUpdateTypes": ["major"],
+      "minimumReleaseAge": "7 days"
+    }
+  ]
+};
