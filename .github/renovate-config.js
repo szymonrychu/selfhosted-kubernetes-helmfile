b44cac7368c4de8494b63238a6bd85d40060262e
Szymon Richert <szymon.rychu@gmail.com>
2023-11-07 09:29:29 +0100

    Cleanup renovate config

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,11 +1,25 @@
 module.exports = {
+  "semanticCommits": "enabled",
+  "extends": ["config:recommended", ":rebaseStalePrs"],
+  "dependencyDashboard": false,
+  "suppressNotifications": ["prIgnoreNotification"],
+  "commitMessageTopic": "{{depName}}",
+  "commitMessageExtra": "to {{newVersion}}",
+  "commitMessageSuffix": "",
+  "rebaseWhen": "conflicted",
   "username": "szymonrichert.pl bot",
+  "pinDigests": true,
+  "automerge": true,
   "gitAuthor": "szymonrichert.pl bot <bot@szymonrichert.pl>",
   "onboarding": false,
   "platform": "github",
   "repositories": ["szymonrychu/helmfile-cluster"],
   "dependencyDashboard": false,
   "packageRules": [
+    {
+      "matchManagers": ["github-actions"],
+      "commitMessageTopic": "github-action {{depName}}",
+    },
     {
       "description": "MinorAutoUpgrade",
       "matchUpdateTypes": ["pin", "digest", "patch", "minor", "major"],
