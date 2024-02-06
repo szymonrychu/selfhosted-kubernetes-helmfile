2df0ddebf4a412e86b59ed3a2091bb8154262bff
Szymon Richert <szymon.rychu@gmail.com>
2024-02-06 09:18:30 +0000

    Add domain verification with cloudflare for ses

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,19 @@ terraform {
       source  = "mrparkers/keycloak"
       version = ">=4.3.1"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">=3.12.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=4.9.0"
+    }
   }
+}
+
+provider "aws" {
+  region = "eu-central-1"
 }
 
 provider "keycloak" {
