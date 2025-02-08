a3a6542a21b532ea253515576a7b8427e3df4b63
Szymon Richert <szymon.rychu@gmail.com>
2025-02-08 13:47:48 +0000

    feat: Setup renovate

--- renovate-config.js
+++ renovate-config.js
@@ -11,8 +11,13 @@ module.exports = {
   "pinDigests": true,
   "automerge": true,
   "gitAuthor": "szymonrichert.pl bot <bot@szymonrichert.pl>",
+  "dependencyDashboard": false,
+  "platform": "gitlab",
+  "endpoint": process.env.CI_API_V4_URL,
+  "token": process.env.BOT_TOKEN,
   "onboarding": false,
-  "platform": "github",
-  "repositories": ["szymonrychu/selfhosted-kubernetes-helmfile"],
-  "dependencyDashboard": false
+  "onboardingConfig": {
+    extends: ['config:recommended'],
+  },
+  "repositories": [process.env.CI_PROJECT_PATH]
 };
